caecad1698af3d9f5f877bcd7ffb6cc34b4b1316
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-04-25 13:29:15 +0530

    update args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use clap::Parser;
+// use clap::{Parser, Command, ArgMatches};
+ use clap::{Parser};
 
 #[derive(Parser)]
 
@@ -7,26 +8,44 @@ use clap::Parser;
 #[command(about="Builds a default ML structure", long_about = None)]
 #[command(author="rvbug")]
 
+#[derive(Debug)]
 struct Arguments {
     /// name of the ML project - default is ml-cookie-project
-    #[arg(short, long, default_value = "ml-cookie-project")]
+    // #[arg(short="--n", long, default_value = "ml-cookie-project")]
+    #[arg(long="name", default_value = "ml-cookie-project")]
     name: Option<String>,
 
     /// path of the ML project
-    #[arg(short, long, default_value = "$HOME")]
+    #[arg(long, default_value = "$HOME")]
     path: Option<String>,
 
     /// venv flag helps create a defailt virtual env named "venv" 
-    #[arg(short, long)]
+    #[arg(long)]
     venv: Option<String>,
 
+
+    // #[arg(long)]
+    // author: Option<String>,
+    //
+    //
+    // #[arg[long]]
+    // about: Option<String>,
+    //
 }
+
 
 
 fn main() {
     // println!("Hello, {:?} {:?}!", args.name, args.path);
     let args = Arguments::parse();
     println!("\n");
+
+    // let matches = Command::new("ML cookie cutter structure")
+    //     .version("1.0")
+    //     .author("rvbug")
+    //     .about("Builds a default ML structure").get_matches();
+
+
 
     match args.name {
         Some(name) => {
@@ -53,6 +72,14 @@ fn main() {
             println!("virtual env will not be created, create it manually");
         }
     }
+
+    // match args.author {
+    //     Some(author) => {
+    //         println!("Author : {}", author);
+    //     }
+    //     None => { 
+    //     }
+    // }
 
 
 }
