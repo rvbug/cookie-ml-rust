8e9b137eeed8bb78c0d45edcf1353f80fcc946ce
Rakesh Venkat <rakesh.venkat@gmail.com>
2024-04-25 11:26:45 +0530

    args added
    using Option in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+use clap::Parser;
+
+#[derive(Parser)]
+
+#[command(name="ML cookie cutter structure")]
+#[command(version="1.0")]
+#[command(about="Builds a default ML structure", long_about = None)]
+#[command(author="rvbug")]
+
+struct Arguments {
+    /// name of the ML project - default is ml-cookie-project
+    #[arg(short, long, default_value = "ml-cookie-project")]
+    name: Option<String>,
+
+    /// path of the ML project
+    #[arg(short, long, default_value = "$HOME")]
+    path: Option<String>,
+
+    /// venv flag helps create a defailt virtual env named "venv" 
+    #[arg(short, long)]
+    venv: Option<String>,
+
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, {:?} {:?}!", args.name, args.path);
+    let args = Arguments::parse();
+    println!("\n");
+
+    match args.name {
+        Some(name) => {
+            println!("Project Name : {:?}", name);
+        }
+        None => {
+            println!("No value providedi for project name");
+        }
+    }
+
+    match args.path {
+        Some(path) => {
+            println!("Path : {}", path);
+        }
+        None => { 
+            println!("No value provided for path");
+        }
+    }
+    match args.venv {
+        Some(venv) => {
+            println!("venv provided is {}", venv);
+        }
+        None => { 
+            println!("virtual env will not be created, create it manually");
+        }
+    }
+
+
 }
